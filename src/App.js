2f8ca06a7d419afdfee8fd27c1e115365b1daf7a
MehdiAghighi <mamaliosezar@gmail.com>
2019-08-07 16:53:24 +0430

    update our menu items to naviaget withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
 
+
+import './App.css';
 import HomePage from "./pages/homepage/homepage.component"
+
+function HatsPage() {
+  return (
+    <h1>HATS PAGE</h1>
+  );
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
